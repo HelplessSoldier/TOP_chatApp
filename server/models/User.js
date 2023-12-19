0333d201ed07e512f0a6671cc7f7d6b4fc16f08c
HelplessSoldier <patrick1234143211@gmail.com>
2023-12-19 15:19:17 -0800

    adding hash to user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
 
 const userSchema = new Schema({
   email: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
-  friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]
-})
+  friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+});
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre("save", (next) => {
+  var user = this;
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(process.env.saltRounds, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
