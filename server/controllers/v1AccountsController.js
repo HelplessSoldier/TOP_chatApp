cdb03dd90c0ef5d9986350ea6a0cc99b1004b203
HelplessSoldier <patrick1234143211@gmail.com>
2023-12-19 15:53:35 -0800

    more work on signup

--- server/controllers/v1AccountsController.js
+++ server/controllers/v1AccountsController.js
@@ -1,12 +1,43 @@
-const asyncHandler = require('express-async-handler');
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
-exports.sign_up_post = asyncHandler(async (req, res) => {
-  const email = req.body.email;
-  const username = req.body.username;
-  const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
+exports.sign_up_post = [
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("E-mail required")
+    .isEmail()
+    .withMessage("Invalid E-mail")
+    .escape(),
 
-  console.log(`email: ${email}\nun: ${username}\npw: ${password}\ncpw: ${confirmPassword}`)
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage('Username required')
+    .escape(),
 
-  res.send('hi')
-})
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be longer than 6 characters")
+    .escape(),
+
+  body("confirmPassword").notEmpty().withMessage("Confirm password required"),
+
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    const confirmPassword = req.body.confirmPassword.value;
+    const password = req.body.password.value;
+    if (confirmPassword !== password) {
+      errors.push({ param: "confirmPassword", msg: "Passwords do not match" })
+    }
+
+    if (errors.length > 0) {
+      res.status(403)
+      res.json({ message: 'Validation errors', errors })
+    }
+
+  })
+];
