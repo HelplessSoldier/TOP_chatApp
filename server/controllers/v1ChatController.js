2231b475e229c4de06d151e84060c4aa598432ea
HelplessSoldier <patrick1234143211@gmail.com>
2023-12-27 14:11:50 -0800

    removed no longer needed async tags

--- server/controllers/v1ChatController.js
+++ server/controllers/v1ChatController.js
@@ -1,28 +1,25 @@
 const WebSocket = require("ws");
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
 const asyncHandler = require("express-async-handler");
 const handleMessage = require("./chatSubModules/handleMessage");
-const handleConnect = require('./chatSubModules/handleConnect');
+const handleConnect = require("./chatSubModules/handleConnect");
 require("dotenv").config();
 
 const server = new WebSocket.Server({ port: 8888 });
 
-server.on(
-  "connection",
-  asyncHandler(async (ws, req) => {
-    handleConnect(ws, req);
+server.on("connection", (ws, req) => {
+  handleConnect(ws, req);
 
-    ws.on("message", (message) => {
-      const parsedMessage = JSON.parse(message);
-      handleMessage(parsedMessage, ws)
-    });
+  ws.on("message", (message) => {
+    const parsedMessage = JSON.parse(message);
+    handleMessage(parsedMessage, ws);
+  });
 
-    ws.on('close', () => console.log('Client disconnected'))
-  })
-);
+  ws.on("close", () => console.log("Client disconnected"));
+});
 
-exports.connect_get = (req, res, next) => {
+exports.connect_get = (req, res) => {
+  // idk why but the server crashes without this route.
+  // it's not actually used for anything but don't delete
+  // without figuring out what's going on with it first.
   res.json({ message: "got into connect_get!" });
 };
-
