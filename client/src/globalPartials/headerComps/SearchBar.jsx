04f7278023fdaefbd6a270c01b63de97310224aa
HelplessSoldier <patrick1234143211@gmail.com>
2023-12-27 13:25:25 -0800

    styling searchbar

--- client/src/globalPartials/headerComps/SearchBar.jsx
+++ client/src/globalPartials/headerComps/SearchBar.jsx
@@ -3,7 +3,10 @@ import './SearchBar.css';
 export default function SearchBar() {
   return (
     <form className="searchbarForm">
-      <input type="search" />
+      <input type="search" className='searchInput' />
+      <button type='submit' className='searchButton'>
+        <img src='./icons/search-svgrepo-com.svg' className='searchIcon'></img>
+      </button>
     </form>
   )
 }
