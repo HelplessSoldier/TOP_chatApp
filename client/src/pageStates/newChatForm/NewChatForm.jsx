3eb4e1a7bc2213fb36337559e333cd581244e966
HelplessSoldier <patrick1234143211@gmail.com>
2024-01-03 15:30:43 -0800

    new chat form styling

--- client/src/pageStates/newChatForm/NewChatForm.jsx
+++ client/src/pageStates/newChatForm/NewChatForm.jsx
@@ -1,26 +1,42 @@
 import "./NewChatForm.css";
 
 export default function NewChatForm({ setPageState }) {
-  const instanceTypes = ["public", "friends", "friendsPlus", "invite", "invitePlus"]
+  const instanceTypes = [
+    "public",
+    "friends",
+    "friendsPlus",
+    "invite",
+    "invitePlus",
+  ];
+
   const handleChatSubmit = (e) => {
     e.preventDefault();
-    console.log(e.data);
+    console.log(e.target);
   };
 
   return (
     <div className="newChatFormRoot">
-      <h2>New Chat</h2>
       <form className="newChatForm" onSubmit={handleChatSubmit}>
-        <label htmlFor="name">Name: </label>
-        <input type="text" name="name" />
-        <label htmlFor="instanceType">Type: </label>
-        <select htmlFor="instanceType" id="instanceType">
-          {instanceTypes.map((type) => {
-            return (
-              <option key={type} value={type}>{type}</option>
-            )
-          })}
-        </select>
+        <h2>New Chat</h2>
+        <div className="labelInputPairContainer">
+          <label htmlFor="name">Name: </label>
+          <input type="text" name="name" />
+        </div>
+        <div className="labelInputPairContainer">
+          <label htmlFor="instanceType">Type: </label>
+          <select htmlFor="instanceType" id="instanceType">
+            {instanceTypes.map((type) => {
+              return (
+                <option key={type} value={type}>
+                  {type}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <button type="submit" className="formButton formButtonOverride">
+          Create
+        </button>
       </form>
     </div>
   );
