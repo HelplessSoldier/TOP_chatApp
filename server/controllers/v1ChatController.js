70e4ba4e2de07ec229dfc17adda11c9243e82db6
HelplessSoldier <patrick1234143211@gmail.com>
2024-01-03 16:56:58 -0800

    can create chat instances!

--- server/controllers/v1ChatController.js
+++ server/controllers/v1ChatController.js
@@ -2,6 +2,10 @@ const WebSocket = require("ws");
 const asyncHandler = require("express-async-handler");
 const handleMessage = require("./chatSubModules/handleMessage");
 const handleConnect = require("./chatSubModules/handleConnect");
+const getCookieFromString = require("../helpers/getCookieFromString");
+const jwt = require("jsonwebtoken");
+const Chat = require("../models/Chat");
+const User = require("../models/User");
 require("dotenv").config();
 
 const server = new WebSocket.Server({ port: 8888 });
@@ -22,9 +26,26 @@ server.on("connection", (ws, req) => {
 });
 
 exports.new_chat_post = asyncHandler(async (req, res, next) => {
-  console.log('got into new_chat_post')
-  res.send('hi c:')
-})
+  try {
+    const body = req.body;
+    const secret = process.env.secret;
+    const token = getCookieFromString(body.token, 'jwt')
+    const userInfo = jwt.verify(token, secret);
+    const user = await User.findById(userInfo.userId);
+    if (!user) {
+      res.json({ message: 'User not found' })
+    }
+    const newChat = new Chat({
+      name: body.chatName,
+      owner: user._id,
+      instanceType: body.chatType,
+    })
+    newChat.participants.push(user._id);
+    await newChat.save();
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 exports.connect_get = (req, res) => {
   // idk why but the server crashes without this route.
