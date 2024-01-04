e393675bc562a0304da68a36fa11bebe2b05c1f6
HelplessSoldier <patrick1234143211@gmail.com>
2024-01-04 15:43:44 -0800

    getting participants in moderation

--- client/src/pageStates/crossPageComps/chatModeration/ModerateChatWindow.jsx
+++ client/src/pageStates/crossPageComps/chatModeration/ModerateChatWindow.jsx
@@ -1,19 +1,38 @@
+import getUsers from "../../../helpers/getUsers";
+import { useEffect, useState } from "react";
 import "./ModerateChatWindow.css";
 
 export default function ModerateChatWindow({ chatObject, setSelectedChat }) {
+  const [participants, setParticipants] = useState([])
+
   const handleCloseButton = () => {
     setSelectedChat(null);
   };
 
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const usersArray = await getUsers(chatObject.participants);
+      setParticipants(usersArray);
+    };
+    fetchUsers();
+  }, [chatObject]);
+
   return (
     <div className="chatModerationRoot">
-      <p>Hi! this is the chat moderation window for chat: {chatObject.name}</p>
-      <button
-        className="closeModerationWindowButton"
-        onClick={handleCloseButton}
-      >
-        <img src="./icons/close-svgrepo-com.svg" className="closeModerationWindowIcon" />
-      </button>
+      <div className="chatModerationHeaderContainer">
+        <h2 className="chatModerationName">{chatObject.name}:</h2>
+        <button
+          className="closeModerationWindowButton"
+          onClick={handleCloseButton}
+        >
+          <img
+            src="./icons/close-svgrepo-com.svg"
+            className="closeModerationWindowIcon"
+          />
+        </button>
+      </div>
+      <div className="participantsContainer">
+      </div>
     </div>
   );
 }
