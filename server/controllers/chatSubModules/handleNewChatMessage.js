87bdad060167284fcd0e047c03dda09c2a00409b
HelplessSoldier <patrick1234143211@gmail.com>
2024-01-08 15:15:19 -0800

    saving message to db and emitting message back to client

--- server/controllers/chatSubModules/handleNewChatMessage.js
+++ server/controllers/chatSubModules/handleNewChatMessage.js
@@ -5,25 +5,43 @@ const getCookieFromString = require("../../helpers/getCookieFromString");
 require("dotenv").config();
 
 async function handleNewChatMessage(message, userSocketMap) {
-  const chatMessage = message.body;
-  const token = getCookieFromString(message.token, "jwt");
-  const userInfo = jwt.verify(token, process.env.secret);
-  const userId = userInfo.userId;
-  const currentUser = await User.findById(userId);
-  const currentChat = await Chat.findById(message.chatId);
-  const participants = currentChat.participants;
+  try {
+    const chatMessage = message.body;
 
-  const messageObject = {
-    message: "New chat message received",
-    sentByUsername: currentUser.username,
-    sentById: currentUser._id,
-    messageBody: chatMessage,
-  };
+    const token = getCookieFromString(message.token, "jwt");
+    const userInfo = jwt.verify(token, process.env.secret);
+    const userId = userInfo.userId;
 
-  participants.map((userId) => {
-    const currentSocket = userSocketMap[userId];
-    currentSocket.send(JSON.stringify(messageObject))
-  });
+    const currentUser = await User.findById(userId);
+    const currentChat = await Chat.findById(message.chatId);
+    const participants = currentChat.participants;
+
+    if (!participants.includes(currentUser._id)) {
+      console.log("user attempted to submit to chat without being participant");
+      return;
+    }
+
+    const messageObject = {
+      sentByUsername: currentUser.username,
+      sentById: currentUser._id,
+      messageBody: chatMessage,
+    };
+
+    const responseObject = {
+      message: "New chat message received",
+      messageObject,
+    };
+
+    currentChat.messages.push(messageObject);
+    await currentChat.save();
+
+    participants.map((userId) => {
+      const currentSocket = userSocketMap[userId];
+      currentSocket.send(JSON.stringify(responseObject));
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 module.exports = handleNewChatMessage;
