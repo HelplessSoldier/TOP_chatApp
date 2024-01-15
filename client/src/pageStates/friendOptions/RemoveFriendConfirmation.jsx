53f65d18849c3b04b2659d3a38c1a1af0d2d707c
HelplessSoldier <patrick1234143211@gmail.com>
2024-01-15 15:10:36 -0800

    done styling remove friend confirmation

--- client/src/pageStates/friendOptions/RemoveFriendConfirmation.jsx
+++ client/src/pageStates/friendOptions/RemoveFriendConfirmation.jsx
@@ -9,18 +9,23 @@ export default function RemoveFriendConfirmation({
     setShowRemoveFriendConfirmation(false);
   };
 
+  const handleConfirm = () => {
+    return;
+  }
+
   return (
     <div className="removeFriendConfirmationContainer">
       <div className="removeFriendConfirmationHeader">
         <h1>Removing friend: {selectedFriend.username}</h1>
-        <button
-          className="removeFriendConfirmationCancelButton"
-          onClick={handleCloseButton}
-        >
-          <img
-            src="./icons/close-svgrepo-com.svg"
-            className="removeFriendConfirmationCancelIcon"
-          />
+        <h2>Are you sure?</h2>
+      </div>
+
+      <div className="removeFriendConfirmationButtonsContainer" >
+        <button className="logoutButton" onClick={handleConfirm}>
+          <img src="./icons/checkmark-svgrepo-com.svg" className="logoutIcon" />
+        </button>
+        <button className="logoutButton" onClick={handleCloseButton}>
+          <img src="./icons/cancel-svgrepo-com.svg" className="logoutIcon" />
         </button>
       </div>
     </div>
