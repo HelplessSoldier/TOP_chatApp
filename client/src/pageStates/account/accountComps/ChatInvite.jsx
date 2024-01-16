6d6086b81d5a7488a9b49c7de4dffe2cbdf4e4ab
HelplessSoldier <patrick1234143211@gmail.com>
2024-01-16 13:21:32 -0800

    done styling chat invites

--- client/src/pageStates/account/accountComps/ChatInvite.jsx
+++ client/src/pageStates/account/accountComps/ChatInvite.jsx
@@ -1,9 +1,35 @@
-import './ChatInvite.css';
+import "./ChatInvite.css";
 
 export default function ChatInvite({ chatObject }) {
+  const handleAccept = () => {
+    console.log('handle accept')
+    return;
+  };
+
+  const handleReject = () => {
+    console.log('handle reject')
+    return;
+  };
+
   return (
-    <div className='chatInviteContainer'>
-      <p>{JSON.stringify(chatObject)}</p>
+    <div className="chatInviteContainer">
+      <p className="chatInviteText">
+        Invited to chat: {chatObject.chatName}. Sent by {chatObject.sentByName}
+      </p>
+      <div className="friendreqButtonContainer">
+        <button className="hiddenChatInviteButton" onClick={handleAccept}>
+          <img
+            src="./icons/checkmark-svgrepo-com.svg"
+            className="chatInviteIcon"
+          />
+        </button>
+        <button className="hiddenChatInviteButton" onClick={handleReject}>
+          <img
+            src="./icons/cancel-svgrepo-com.svg"
+            className="chatInviteIcon"
+          />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
