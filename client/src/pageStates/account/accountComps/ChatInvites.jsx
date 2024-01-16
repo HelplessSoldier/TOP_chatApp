3a10c197f987900ac9e5d7e63b59fe18e7e477c5
HelplessSoldier <patrick1234143211@gmail.com>
2024-01-16 13:12:46 -0800

    chat invite header style

--- client/src/pageStates/account/accountComps/ChatInvites.jsx
+++ client/src/pageStates/account/accountComps/ChatInvites.jsx
@@ -1,12 +1,14 @@
-import './ChatInvites.css';
 import ChatInvite from './ChatInvite';
+import "./AccountSections.css";
 
 export default function ChatInvites({ ChatInvites, userObject }) {
   return (
-    <>
+    <div className='accountSectionContainer'>
+      <h2 className='accountSectionHeader'>Chat invites:</h2>
+      <hr />
       {userObject.chatInvites.map((chatObject) => (
         <ChatInvite key={chatObject._id} chatObject={chatObject} />
       ))}
-    </>
+    </div>
   )
 }
