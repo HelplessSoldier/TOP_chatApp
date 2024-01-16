ad551b492c83d496871222bd534c873b4867fa52
HelplessSoldier <patrick1234143211@gmail.com>
2024-01-16 15:37:38 -0800

    can join public lobbies from search

--- server/controllers/chatSubModules/handleJoinPublicChat.js
+++ server/controllers/chatSubModules/handleJoinPublicChat.js
@@ -1,5 +1,44 @@
+const jwt = require("jsonwebtoken");
+const User = require("../../models/User");
+const Chat = require("../../models/Chat");
+require("dotenv").config();
+
 async function handleJoinPublicChat(message, socket) {
-  console.log('got into handleJoinPublicChat');
+  try {
+    const userId = jwt.verify(message.jwt, process.env.secret).userId;
+    const user = await User.findById(userId);
+    const chat = await Chat.findById(message.chatId);
+
+    if (!chat.instanceType === "public") {
+      socket.send(
+        JSON.stringify({
+          message: "Failed to join chat",
+          detail: "Chat not public",
+        })
+      );
+      return;
+    }
+
+    user.chats.push(chat._id);
+    chat.participants.push(user._id);
+
+    await Promise.all([user.save(), chat.save()]);
+
+    socket.send(
+      JSON.stringify({
+        message: "Successfully joined chat",
+        chatId: chat._id,
+      })
+    );
+  } catch (err) {
+    console.error(err);
+    socket.send(
+      JSON.stringify({
+        message: "Failed to join chat",
+        detail: "Internal server error",
+      })
+    );
+  }
 }
 
 module.exports = handleJoinPublicChat;
