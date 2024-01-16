b99d0c499db7887358a4db826f2c8cdb3184eeaa
HelplessSoldier <patrick1234143211@gmail.com>
2024-01-16 14:13:58 -0800

    working on adding chat from invite

--- client/src/pageStates/account/accountComps/ChatInvite.jsx
+++ client/src/pageStates/account/accountComps/ChatInvite.jsx
@@ -1,13 +1,16 @@
 import "./ChatInvite.css";
+import { useState } from 'react';
 
 export default function ChatInvite({ chatObject, userObject, socket }) {
+  const [displayed, setDisplayed] = useState(true);
+
   const handleAccept = () => {
     const message = JSON.stringify({
       message: "Chat invite accepted",
       userId: userObject._id,
       chatId: chatObject.chatid,
     });
-    socket.send(message)
+    socket.send(message);
     return;
   };
 
@@ -17,29 +20,43 @@ export default function ChatInvite({ chatObject, userObject, socket }) {
       userId: userObject._id,
       chatId: chatObject.chatid,
     });
-    socket.send(message)
+    socket.send(message);
     return;
   };
 
-  return (
-    <div className="chatInviteContainer">
-      <p className="chatInviteText">
-        Invited to chat: {chatObject.chatName}. Sent by {chatObject.sentByName}
-      </p>
-      <div className="friendreqButtonContainer">
-        <button className="hiddenChatInviteButton" onClick={handleAccept}>
-          <img
-            src="./icons/checkmark-svgrepo-com.svg"
-            className="chatInviteIcon"
-          />
-        </button>
-        <button className="hiddenChatInviteButton" onClick={handleReject}>
-          <img
-            src="./icons/cancel-svgrepo-com.svg"
-            className="chatInviteIcon"
-          />
-        </button>
+  socket.addEventListener("message", (event) => {
+    const msg = JSON.parse(event.data);
+    if (msg.message === 'Remove chat invite') {
+      const chatToRemoveId = msg.chatId;
+      if (chatToRemoveId === chatObject._id) {
+        setDisplayed(false);
+      }
+    }
+  });
+
+  if (displayed) {
+    return (
+      <div className="chatInviteContainer">
+        <p className="chatInviteText">
+          Invited to chat: {chatObject.chatName}. Sent by {chatObject.sentByName}
+        </p>
+        <div className="friendreqButtonContainer">
+          <button className="hiddenChatInviteButton" onClick={handleAccept}>
+            <img
+              src="./icons/checkmark-svgrepo-com.svg"
+              className="chatInviteIcon"
+            />
+          </button>
+          <button className="hiddenChatInviteButton" onClick={handleReject}>
+            <img
+              src="./icons/cancel-svgrepo-com.svg"
+              className="chatInviteIcon"
+            />
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 }
--- server/controllers/chatSubModules/handleChatInviteAccept.js
+++ server/controllers/chatSubModules/handleChatInviteAccept.js
@@ -1,5 +1,62 @@
+const User = require("../../models/User.js");
+const Chat = require("../../models/Chat.js");
+
 async function handleChatInviteAccept(message, socket) {
-  console.log('got into handle chat invite accept')
+  try {
+    const user = await User.findById(message.userId);
+    const chat = await Chat.findById(message.chatId);
+
+    if (!user || !chat) {
+      socket.send(
+        JSON.stringify({
+          message: "Failed to add chat",
+          detail: "User or chat not found",
+        })
+      );
+      console.error(
+        `One not found: user not found -> ${!user} chat not found -> ${!chat}`
+      );
+      return;
+    }
+
+    if (user.chats.includes(chat._id)) {
+      socket.send(
+        JSON.stringify({
+          message: "Failed to add chat",
+          detail: "User already participant of chat",
+        })
+      );
+      console.error("User already has chat")
+      return;
+    }
+
+    // add chats id to user and remove the invite
+    user.chats.push(chat._id);
+
+    // this isn't removing the chat invite?
+    console.log("Before filter: ", user.chatInvites);
+    user.chatInvites = user.chatInvites.filter(
+      (chatInvite) => chatInvite.chatid.toString() !== chat._id.toString()
+    );
+    console.log("After filter: ", user.chatInvites);
+
+    await user.save();
+
+    socket.send(
+      JSON.stringify({
+        message: "Remove chat invite",
+        chatId: message.chatId,
+      })
+    );
+  } catch (err) {
+    console.error(err);
+    socket.send(
+      JSON.stringify({
+        message: "Failed to add chat",
+        detail: "Internal server error",
+      })
+    );
+  }
 }
 
 module.exports = handleChatInviteAccept;
